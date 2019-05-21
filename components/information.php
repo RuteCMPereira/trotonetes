79f4ecc496d1b5ddc712bea10131496b03b79e7b
rutep <rutecmpereira99@gmail.com>
2019-05-21 11:18:52 +0100

    //ADD LOADER

---
+++ components/information.php
@@ -0,0 +1,15 @@
+<section class="row clip-me-div justify-content-center p-0" id="component">
+
+    <div class="bg-danger col-7 text-center "style="border-radius: 50px;">
+       <p class="m-0 py-2">INFORMAÇÕES</p>
+    </div>
+    <div class="overflow-auto col-11 pb-5" style=" max-height: 100%; color: #b9bbbe;">
+        <div>
+
+        </div>
+    </div>
+</section>
+
+
+
+
--- components/loader.php
+++ components/loader.php
@@ -3,7 +3,7 @@
     </div>
     <div class="col-12 hidden-md-down  position-absolute h-100 open_close_animation bg-ligth-lantern text-center my-auto align-middle pt-5 font-ligth">
         <img src="images/logo_no_shadow.png" height="30%">
-
+        <br>
         <img src="images/MA.png" class="pt-5" height="23%">
 
 
@@ -19,3 +19,95 @@
     </div>
 </div>
 
+<style>
+    /*CSS DO LOADER*/
+
+    /* LOADING DOTS*/
+    .spinner {
+        margin: 100px auto 0;
+        width: 70px;
+        text-align: center;
+    }
+
+    .spinner > div {
+        width: 18px;
+        height: 18px;
+        background-color: #EFD0BC;
+
+        border-radius: 100%;
+        display: inline-block;
+        -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+        animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+    }
+
+    .spinner .bounce1 {
+        -webkit-animation-delay: -0.32s;
+        animation-delay: -0.32s;
+    }
+
+    .spinner .bounce2 {
+        -webkit-animation-delay: -0.16s;
+        animation-delay: -0.16s;
+    }
+
+    @-webkit-keyframes sk-bouncedelay {
+        0%, 80%, 100% {
+            -webkit-transform: scale(0)
+        }
+        40% {
+            -webkit-transform: scale(1.0)
+        }
+    }
+
+    @keyframes sk-bouncedelay {
+        0%, 80%, 100% {
+            -webkit-transform: scale(0);
+            transform: scale(0);
+        }
+        40% {
+            -webkit-transform: scale(1.0);
+            transform: scale(1.0);
+        }
+    }
+
+    /*LANTERNA ANIMATION*/
+
+    .open_close_animation {
+
+        animation: open_close 7s infinite;
+        animation-timing-function: linear;
+    }
+
+    @keyframes open_close {
+
+        0% {clip-path: polygon(0 0, 100% 0, 50% 0%, 50% 88%, 50% 0%);}
+        5%{clip-path: polygon(0 0, 100% 0, 50% 0%, 50% 88%, 50% 0%);}
+        8% {clip-path: polygon(0 0, 100% 0, 100% 0%, 50% 88%, 0 0%);}
+        10% {clip-path: polygon(0 0, 100% 0, 100% 60%, 50% 88%, 0 60%);}
+        90% {clip-path: polygon(0 0, 100% 0, 100% 60%, 50% 88%, 0 60%);}
+        92% {clip-path: polygon(0 0, 100% 0, 100% 0%, 50% 88%, 0 0%);}
+        95%{clip-path: polygon(0 0, 100% 0, 50% 0%, 50% 88%, 50% 0%);}
+        100% {clip-path: polygon(0 0, 100% 0, 50% 0%, 50% 88%, 50% 0%);}
+
+
+
+    }
+
+
+    /* BACKGROUND COLORS */
+    .bg-gradient-1 {
+
+        height: 100%;
+        width: 100%;
+        background: linear-gradient(0deg, rgba(35, 15, 27, 1) 0%, rgba(56, 20, 39, 1) 27%);
+
+    }
+
+
+    .bg-ligth-lantern {
+
+        background-color: #542D44;
+    }
+
+    /*FIM DO CSS DO LOADER*/
+</style>
