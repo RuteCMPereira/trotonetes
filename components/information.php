03c80c16b1601836117350e6b8327d49a0eea1be
rutep <rutecmpereira99@gmail.com>
2019-05-22 00:24:58 +0100

    //DELETE STUFF

--- components/information.php
+++ components/information.php
@@ -10,34 +10,46 @@
     </section>
 
 
-    <section class="row p-0 justify-content-start overflow-auto" style="max-height: 120%;">
+    <section class="row p-0 justify-content-center" style="max-height: 120%; overflow-y: auto">
 
-        <div class="col-10 bg-danger p-2 justify-content-between d-flex mt-3 " style="border-radius: 0px 50px 50px 0px">
-            <p class="m-0 pl-3">hhhhhh</p>
-            <a onclick="myFunction('info1') " id="toggle_plus">
-                <i class="fas fa-plus bg-light p-2" style="border-radius: 50px"></i>
-            </a>
-        </div>
-        <div class="col-9 bg-light p-2 justify-content-between begin" id="info1" style="border-radius: 0px 0 20px 0px">
-            <p class="m-0 pl-3">234776145</p>
-        </div>
-        <div class="col-10 bg-danger p-2 justify-content-between d-flex mt-3 " style="border-radius: 0px 50px 50px 0px">
-            <p class="m-0 pl-3">hhhhhh</p>
-            <a onclick="myFunction('info2')">
-                <i class="fas fa-plus bg-light p-2" style="border-radius: 50px"></i>
-            </a>
-        </div>
-        <div class="col-9 bg-light p-2 justify-content-between begin" id="info2" style="border-radius: 0px 0 20px 0px">
-            <p class="m-0 pl-3">234776145</p>
-        </div>
-        <div class="col-10 bg-danger p-2 justify-content-between d-flex mt-3 " style="border-radius: 0px 50px 50px 0px">
-            <p class="m-0 pl-3">hhhhhh</p>
-            <a onclick="myFunction('info3')">
-                <i class="fas fa-plus bg-light p-2" style="border-radius: 50px"></i>
-            </a>
-        </div>
-        <div class="col-9 bg-light p-2 justify-content-between begin" id="info3" style="border-radius: 0px 0 20px 0px">
-            <p class="m-0 pl-3">234776145</p>
+        <div class="col-10  text-danger text-wrap ">
+            <p>
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat a nibh in sollicitudin.
+                Maecenas sit amet consequat nibh. Cras eu diam scelerisque dolor suscipit euismod non ut augue. Vivamus
+                laoreet porta urna, sed tempor est bibendum ut. In at vestibulum metus. Proin sed fringilla lacus. Nunc
+                porta elit tellus, a blandit odio fringilla sed. Aliquam ornare massa non arcu euismod volutpat. Etiam
+                tempor, tellus et molestie rhoncus, libero nunc sollicitudin ipsum, id pulvinar nibh orci nec turpis.
+                Aliquam mattis dolor vitae diam fermentum, nec facilisis massa lacinia. Aenean at enim ultrices,
+                sagittis orci semper, gravida tellus.
+
+                Curabitur purus felis, posuere non posuere et, imperdiet eu nunc. Phasellus commodo vestibulum quam quis
+                pulvinar. Morbi luctus volutpat erat. Aliquam erat volutpat. Aenean laoreet lectus non ante ullamcorper
+                rhoncus. Vivamus congue nibh vel nisi finibus, quis consequat turpis dignissim. Aliquam erat volutpat.
+                Nullam auctor pellentesque tortor id convallis. Pellentesque egestas interdum nibh. Nam vehicula
+                hendrerit urna, at aliquam tellus consectetur ut. Sed elementum lobortis purus in varius. Etiam volutpat
+                velit feugiat dui egestas, non tempor felis volutpat. Morbi vel nibh libero. Cras eget molestie lorem.
+                Nullam dictum, lectus ornare tempor commodo, mauris purus feugiat orci, eu condimentum lacus eros sit
+                amet ante.
+
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam mattis
+                vehicula leo, a placerat nisl condimentum vel. In maximus nisl vel quam iaculis, sit amet convallis erat
+                faucibus. Aliquam ornare, leo sit amet auctor ornare, ipsum ante faucibus purus, et blandit lacus ligula
+                ac lacus. Duis volutpat quam id ante mattis viverra. Nullam imperdiet varius risus eget laoreet.
+                Curabitur sagittis nibh vel urna consectetur tincidunt.
+
+                Vestibulum malesuada hendrerit odio, et dictum est sagittis quis. Fusce dolor ex, tempus lacinia
+                ultrices sit amet, euismod vitae ipsum. Integer consectetur nisi vel dolor accumsan, eget consectetur
+                sem gravida. Nam consectetur condimentum sem non egestas. Sed odio velit, sodales a eros in, volutpat
+                elementum nisi. Integer porta sit amet justo ac egestas. Nulla facilisi. Proin finibus cursus sem, vitae
+                suscipit augue posuere vel. Aenean interdum velit in vulputate luctus. Pellentesque lobortis, nunc vitae
+                dictum egestas, enim nunc semper velit, sit amet dictum libero erat eu lorem. Aenean tincidunt est nec
+                tempor fermentum. Etiam maximus, dui ultricies cursus condimentum, tortor elit bibendum est, quis
+                suscipit quam magna vitae erat. Nullam neque ligula, sagittis sit amet ex eget, finibus rhoncus urna.
+                Praesent neque tortor, sollicitudin non nisl in, ultrices suscipit est.
+
+
+            </p>
         </div>
 
 
