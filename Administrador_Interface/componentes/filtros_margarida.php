6b33eba42dc6f5053d0b46f45cb8a21ee1b17a18
Margarida <margaridaalves@ua.pt>
2019-06-10 18:05:19 +0100

    Margarida: adicionei muitas coisas

--- Administrador_Interface/componentes/filtros_margarida.php
+++ Administrador_Interface/componentes/filtros_margarida.php
@@ -1,31 +1,46 @@
 <?php
-//Conexão com o banco:
-include("../admin/configdb.php");
+$host = "localhost";
+$user = "root";
+$pass = "";
+$banco = "filtro_questoes";
+$connection = mysqli_connect($host, $user, $pass, $banco) or die ("Erro ao conectar-se");
+mysqli_select_db($connection, $banco) or die ("Não foi possível selecionar o banco");
 
-//Recupera resultados
-$sql="SELECT * FROM imoveis WHERE finalidade LIKE '%" . $busca . "%' OR tipo LIKE '%" . $busca . "%' OR cidade LIKE '%" . $busca . "%' order by valor LIMIT $inicio, $npp";
-$res=@mysql_query($sql, $conexao) or die("Erro no MySQL:<br/>" . mysql_errno());
+//////////////////////DISCIPLINAS//////////////////////////////////////////////
+$sql_disciplina = "SELECT * FROM tabela_disciplina ORDER BY Disciplina ASC";
+$pega_disciplina = mysqli_query($connection, $sql_disciplina);
 
-//exibe resultados encontrados no Banco de Dados
-while(list($codigo, $tipo, $area, $finalidade, $complemento, $endereço, $bairro, $cidade, $valor, $telefone, $informações, $foto, $lat, $lon)=mysql_fetch_array($res))
+////////////////////////////////////////BANCAS/////////////////////////////////
+$sql_banca="SELECT * FROM tabela_banca ORDER BY Banca ASC";
+$pega_banca = mysqli_query($connection,$sql_banca);
+
+////////////////////////////////////////INSTITUIÇÃO/////////////////////////////////
+$sql_instituicao="SELECT * FROM tabela_instituicao ORDER BY Instituicao ASC";
+$pega_instituicao = mysqli_query($connection,$sql_instituicao);
+
+////////////////////////////////////////CARGO/////////////////////////////////
+$sql_cargo="SELECT * FROM tabela_cargo ORDER BY Cargo ASC";
+$pega_cargo = mysqli_query($connection,$sql_cargo);
+
+////////////////////////////////////////ANO////////////////////////////////////
+$sql_ano="SELECT * FROM tabela_ano ORDER BY Ano ASC";
+$pega_ano = mysqli_query($connection,$sql_ano);
+
+//////////////////////NÍVEL//////////////////////////////////////////////
+$sql_nivel = "SELECT * FROM tabela_nivel ORDER BY id_nivel ASC";
+$pega_nivel = mysqli_query($connection, $sql_nivel);
+
+////////////////////////////////TRAZ AS QUESTÕES DE ACORDO COM O CRITÉRIO/////
+if(!empty($_POST['bt_enviar']))
 {
-
-//MOSTRA OS RESULTADOS DENTRO DESTA TABELA HTML
-
-    echo "
-apenas teste de resultados
-$tipo - $area<br>
-$finalidade<br>
-$complemento<br>
-$endereço<br>
-$bairro<br>
-$cidade<br>
-$valor<br>
-$telefone<br>
-$informações";
-
+    $disciplina = (empty($_POST['Disciplina']))? 'null' : $_POST['Disciplina'];
+    $banca = (empty($_POST['Banca']))? 'null' : $_POST['Banca'];
+    $instituicao = (empty($_POST['Instituicao']))? 'null' : $_POST['Instituicao'];
+    $cargo = (empty($_POST['Cargo']))? 'null' : $_POST['Cargo'];
+    $ano = (empty($_POST['Ano']))? 'null' : $_POST['Ano'];
+    $nivel = (empty($_POST['Nivel']))? 'null' : $_POST['Nivel'];
+    $sql_questao="SELECT * FROM tabela_questao WHERE ( id_disciplina = $disciplina OR $disciplina = 0) AND (id_banca = $banca OR $banca = 0) AND (id_ano = $ano OR $ano = 0) AND (id_nivel = $nivel OR $nivel = 0) AND (id_instituicao = $instituicao OR $instituicao = 0) AND (id_cargo = $cargo OR $cargo = 0)";
+    $seleciona_questao = mysqli_query($connection,$sql_questao);
 }
-
-mysql_free_result($res);
-
 ?>
+
