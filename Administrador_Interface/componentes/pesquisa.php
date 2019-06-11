44964086f2e3a9dcbf23fddd10f1cbc9b8e22542
Margarida <margaridaalves@ua.pt>
2019-06-11 11:01:13 +0100

    Margarida: adicionei muitas coisas

--- Administrador_Interface/componentes/pesquisa.php
+++ Administrador_Interface/componentes/pesquisa.php
@@ -1,9 +1,14 @@
 <div class="container-fluid">
     <div class="d-sm-flex col-12 align-items-center justify-content-end pesquisa px-5">
         <div id="wrap">
+
+            <?php
+
+            if (isset())
+            ?>
             <form action="" autocomplete="on">
-                <input id="search" name="search" type="text" placeholder="Pesquisa"><input
-                    id="search_submit" value="Rechercher" type="submit">
+                <input id="search" type="text" placeholder="Pesquisa">
+                <input type="submit">
             </form>
         </div>
         <div><img src="img/filter.svg" onclick="openNav()" class="px-3 filter" height="50px"
