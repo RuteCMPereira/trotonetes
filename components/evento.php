887361f9baa7bdd62bc3e7f92706a10aa0983aad
hanna <laurarsantos99@ua.pt>
2019-06-14 14:51:46 +0100

    added eventoooooooo

---
+++ components/evento.php
@@ -0,0 +1,17 @@
+
+    <div class="dropdown-container evento">
+        <input type="checkbox" id="drop" />
+        <label for="drop">JUNHO</label>
+            <ul class="content evento_content">
+                <a><li class="text-center julho">JULHO</li></a>
+                <a><li class="text-center mes">AGOSTO</li></a>
+                <a><li class="text-center mes">SETEMBRO</li></a>
+                <a><li class="text-center mes">OUTUBRO</li></a>
+                <a><li class="text-center mes">NOVEMBRO</li></a>
+                <a><li class="text-center dezembro">DEZEMBRO</li></a>
+            </ul>
+    </div>
+
+    <div>
+        <img class="fotoevento" src="images/que_musica.jpg">
+    </div>
